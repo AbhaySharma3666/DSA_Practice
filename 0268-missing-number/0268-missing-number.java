f9424cf23f0748fd6df62036b3ee5209104e9b89
Abhay Sharma <154610193+AbhaySharma3666@users.noreply.github.com>
2026-06-23 21:07:13 +0530

    Time: 1 ms (29.37%), Space: 47.3 MB (47.96%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,26 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        int i = 0;
         int n = nums.length;
-        // sum of 0 to n
-        int sum = (n*(n+1))/2; 
-        int arrsum= 0;
-        for(int ele : nums){
-            arrsum += ele;
+    
+        while (i < n) {
+            // Correct position is nums[i] == i
+            // Only swap if the number is within [0, n-1] 
+            // AND it's not already in the correct place
+            if (nums[i] < n && nums[i] != i) {
+                swap(nums, i, nums[i]);
+            } else i++;
         }
-        return sum - arrsum;
+    
+        // After sorting, the index where nums[i] != i is your missing number
+        for (int j = 0; j < n; j++) if (nums[j] != j) return j;
+    
+        return n;
+    }
+
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
